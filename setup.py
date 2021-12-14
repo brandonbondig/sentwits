6df4156fbfbc66ec6efe4c0592be07bde4117896
Brandon Bondig <brandon@bondig.dk>
2021-12-14 23:06:07 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sentwits',
-    version='0.0.1',
+    version='0.0.2',
     description='Real-time sentiment Stocktwits analysis tool',
     url='https://github.com/mcdallas/wallstreet',
     author='Brandon Bondig',
